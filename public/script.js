788ceb8029e6a6649bcc7dafae0d989ece64fab1
Teodor DEACONU <tdeaconu@bbu.bitdefender.biz>
2021-04-23 07:27:54 +0300

    Assist Trading App v1

--- public/script.js
+++ public/script.js
@@ -1,6 +1,17 @@
 const socket = io.connect("http://localhost:4001");
 
+const rankingBody= document.querySelector("#table-body");
+
 socket.on('tabel-data', (data) => {
-    console.log(data);
+    rankingBody.innerHTML = '';
+    for(const tickerData of data) {
+        const tr = document.createElement('tr');
+        for(const property in tickerData) {
+            const td = document.createElement('td');
+            td.textContent = tickerData[property];
+            tr.appendChild(td);
+        }
+        rankingBody.appendChild(tr);
+    }
 })
 
